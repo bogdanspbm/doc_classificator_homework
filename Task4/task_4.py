bd4cf18c8b56f25b122d4b31eb775737c516df48
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2021-10-31 13:48:01 +0300

    1

--- Task4/task_4.py
+++ Task4/task_4.py
@@ -19,8 +19,9 @@ Y = D[:, -1]
 Y = np.reshape(Y, [1000, 1])
 
 
-def plot_loss(x, y):
-    plt.plot(x, y)
+def plot_loss(x, y_1, y_2):
+    plt.plot(x, y_1, label="Train")
+    plt.plot(x, y_2, label="Test")
     plt.xlabel('Lambda')
     plt.ylabel('Loss')
     plt.legend()
@@ -28,14 +29,18 @@ def plot_loss(x, y):
     plt.show()
 
 
-DIMENSIONS = 4
+DIMENSIONS = int(X.shape[1])
+ALPHA = 1e-3
+TRAINING_STEPS = 1000
+TRAIN_RATION = 0.8
 
-noise = lambda: np.random.normal(0, 10)  # some noise
+train_size = int(len(X) * TRAIN_RATION)
+test_size = len(X) - train_size
 
-ALPHA = 1e-4  # learning rate
-TRAINING_STEPS = 10000
-
-_train_size = len(X)
+train_X = X[:train_size, :]
+train_Y = Y[:train_size, :]
+test_X = X[train_size:, :]
+test_Y = Y[train_size:, :]
 
 
 def calc_theta(LAMBDA=0.5):
@@ -43,16 +48,20 @@ def calc_theta(LAMBDA=0.5):
     graph = tf.Graph()
     with graph.as_default():
         # declare graph inputs
-        x_train = tf.compat.v1.placeholder(tf.float32, shape=(_train_size, DIMENSIONS))
-        y_train = tf.compat.v1.placeholder(tf.float32, shape=(_train_size, 1))
+        x_train = tf.compat.v1.placeholder(tf.float32, shape=(train_size, DIMENSIONS))
+        y_train = tf.compat.v1.placeholder(tf.float32, shape=(train_size, 1))
+        x_test = tf.compat.v1.placeholder(tf.float32, shape=(test_size, DIMENSIONS))
+        y_test = tf.compat.v1.placeholder(tf.float32, shape=(test_size, 1))
         theta = tf.Variable([[0.0] for _ in range(DIMENSIONS)])
         theta_0 = tf.Variable([[0.0]])  # don't forget the bias term!
         # forward propagation
         train_prediction = tf.matmul(x_train, theta) + theta_0
+        test_prediction = tf.matmul(x_test, theta) + theta_0
         # cost function and optimizer
         train_cost = (tf.nn.l2_loss(train_prediction - y_train) + LAMBDA * tf.matmul(tf.transpose(theta),
-                                                                                     theta)) / float(
-            _train_size)
+                                                                                     theta)) / float(train_size)
+        test_cost = (tf.nn.l2_loss(test_prediction - y_test) + LAMBDA * tf.matmul(tf.transpose(theta), theta)) / float(
+            test_size)
         optimizer = tf.compat.v1.train.GradientDescentOptimizer(ALPHA).minimize(train_cost)
 
     # run the computation
@@ -61,22 +70,24 @@ def calc_theta(LAMBDA=0.5):
         print("initialized");
         print(theta.eval())
         for step in range(TRAINING_STEPS):
-            _, train_c = s.run([optimizer, train_cost], feed_dict={x_train: X, y_train: Y})
-            if (step % 100 == 0):
+            _, train_c, test_c = s.run([optimizer, train_cost, test_cost],
+                                       feed_dict={x_train: train_X, y_train: train_Y, x_test: test_X, y_test: test_Y})
+            if step % 100 == 0:
                 pass
 
         # PARAMETERS_GRADDESC = tf.concat(0, [theta_0, theta]).eval()
         # print("Solution for parameters:\n", PARAMETERS_GRADDESC)
         print("\nAfter", TRAINING_STEPS, "iterations:")
         print("   train cost =", train_c)
-        return train_c
+        return train_c[0][0], test_c[0][0]
 
 
 LAMBDAS = [0, 0.01, 0.025, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.75, 0.9, 0.95, 0.99, 1, 2, 5, 10]
-LOSSES = []
-
+LOSSES_TRAIN = []
+LOSSES_TEST = []
 for lmbd in LAMBDAS:
-    LOSSES.append(calc_theta(lmbd)[0][0])
+    train_l, test_l = calc_theta(lmbd)
+    LOSSES_TRAIN.append(train_l)
+    LOSSES_TEST.append(test_l)
 
-
-plot_loss(LAMBDAS, LOSSES)
+plot_loss(LAMBDAS, LOSSES_TRAIN, LOSSES_TEST)
