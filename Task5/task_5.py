4fe550ea4fef84d0ffd773ece5beb355641d7f2a
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2021-11-19 19:02:36 +0300

    1

--- Task5/task_5.py
+++ Task5/task_5.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
 tmp_data = pd.read_csv('data.csv')
 data = pd.DataFrame(tmp_data['date'].str.split('T', 1).tolist(),
                     columns=['date', 'time'])
 data['message'] = tmp_data['message']
+data = data.dropna()
+data = data[data['message'].str.contains('Ж|М|м|ж')]
+data = data[data['message'].str.contains('1|2|3|4|5|6|7|8|9|0')]
+data['tmp'] = data['message'].str.extract('([МЖмж].[0123456789].)', flags=re.IGNORECASE, expand=False).str.lower()
+data['gender'] = data['tmp'].str.extract('([МЖмж])', flags=re.IGNORECASE, expand=False).str.lower()
+data['age'] = data['tmp'].str.extract('([0123456789][0123456789])', flags=re.IGNORECASE, expand=False).str.lower()
+data = data.dropna()
 print(data)
 message_per_date = data.groupby('date').size()
 print(message_per_date)
