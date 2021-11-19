4d2ee558a8e55dd2eb88910dfc9ec23560d4f5f8
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2021-11-19 20:37:39 +0300

    1

--- Task5/task_5.py
+++ Task5/task_5.py
@@ -1,6 +1,9 @@
+from operator import le
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+import numpy as np
 
 tmp_data = pd.read_csv('data.csv')
 data = pd.DataFrame(tmp_data['date'].str.split('T', 1).tolist(),
@@ -13,9 +16,19 @@ data['tmp'] = data['message'].str.extract('([МЖмж].[0123456789].)', flags=re
 data['gender'] = data['tmp'].str.extract('([МЖмж])', flags=re.IGNORECASE, expand=False).str.lower()
 data['age'] = data['tmp'].str.extract('([0123456789][0123456789])', flags=re.IGNORECASE, expand=False).str.lower()
 data = data.dropna()
-print(data)
-message_per_date = data.groupby('date').size()
-print(message_per_date)
-message_per_date.plot()
-plt.gcf().autofmt_xdate()
+
+data = data[data['message'].str.contains('bau|BAU|Bau')]
+data['bau'] = data['message'].str.extract('(...[0123456789.,][0123456789][0123456789].bau)', flags=re.IGNORECASE,
+                                          expand=False).str.lower()
+data['bau'] = data['bau'].str.replace(' bau', '')
+data['bau'] = data['bau'].str.replace(',', '.')
+data['bau'] = data['bau'].str.replace('.[0123456789][.]', '')
+data['bau'] = data['bau'].str.replace('(\D)', '')
+data = data[data['bau'].str.len() < 5]
+data['bau'] = pd.to_numeric(data['bau'], errors='ignore')
+data = data.dropna()
+
+data_for_plot = data.groupby(['age'])['bau'].mean().reset_index()
+data_for_plot.plot()
 plt.show()
+print(data_for_plot)
